bc5f9958bb5efac7bb19834569b1cae4e341a469
HabiburRahmanAnik <habib264521@gmail.com>
2023-01-15 10:05:21 +0600

    protect private route

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,27 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+import { getSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        router.push('/auth');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
